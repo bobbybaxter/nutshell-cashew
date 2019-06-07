5b8429ffd720c57c430d191a1992bed883004eec
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-06 19:23:35 -0500

    auth redo

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,18 @@
+import firebase from 'firebase/app';
+
+import auth from './components/auth/auth';
+
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
+import navbar from './components/navbar/navbar';
+import authData from './helpers/data/auth-data';
 
 const init = () => {
-  console.error('test');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.authStringBuilder();
+  authData.checkLoginStatus();
+  navbar.navbarEvents();
 };
 
 init();
