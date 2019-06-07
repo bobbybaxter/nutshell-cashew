126aaea838e60622e016dfdd232580d1b2430240
Samuel T. Mendoza <samuel.mendoza@uniguest.com>
2019-06-07 15:11:10 -0500

    showing and printing diary entries upon diary nav link click

--- src/javascripts/helpers/data/auth-data.js
+++ src/javascripts/helpers/data/auth-data.js
@@ -1,9 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import diary from '../../components/diary/diary';
+
 const movieNavbar = document.getElementById('navbar-button-movie-history');
 const authNavbar = document.getElementById('navbar-button-auth');
 const logoutNavbar = document.getElementById('navbar-button-logout');
+const diaryNav = document.getElementById('navbar-button-diary');
 const authDiv = document.getElementById('auth');
 
 const checkLoginStatus = () => {
@@ -13,11 +16,14 @@ const checkLoginStatus = () => {
       authNavbar.classList.add('hide');
       logoutNavbar.classList.remove('hide');
       authDiv.classList.add('hide');
+      diaryNav.classList.remove('hide');
+      diaryNav.addEventListener('click', diary.initDiary);
     } else {
       movieNavbar.classList.add('hide');
       authNavbar.classList.remove('hide');
       logoutNavbar.classList.add('hide');
       authDiv.classList.remove('hide');
+      diaryNav.classList.add('hide');
     }
   });
 };
