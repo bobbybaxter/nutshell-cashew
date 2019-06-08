f4cc666cd25591d0dded27bad28fcac73c748488
Bob <bob.e.baxter@gmail.com>
2019-06-08 10:53:13 -0500

    initial sidebar setup complete

--- src/javascripts/helpers/data/auth-data.js
+++ src/javascripts/helpers/data/auth-data.js
@@ -3,7 +3,7 @@ import 'firebase/auth';
 
 import diary from '../../components/diary/diary';
 
-const movieNavbar = document.getElementById('navbar-button-movie-history');
+const homeNavbar = document.getElementById('navbar-button-home');
 const authNavbar = document.getElementById('navbar-button-auth');
 const logoutNavbar = document.getElementById('navbar-button-logout');
 const diaryNav = document.getElementById('navbar-button-diary');
@@ -12,14 +12,14 @@ const authDiv = document.getElementById('auth');
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      movieNavbar.classList.remove('hide');
+      homeNavbar.classList.remove('hide');
       authNavbar.classList.add('hide');
       logoutNavbar.classList.remove('hide');
       authDiv.classList.add('hide');
       diaryNav.classList.remove('hide');
       diaryNav.addEventListener('click', diary.initDiary);
     } else {
-      movieNavbar.classList.add('hide');
+      homeNavbar.classList.add('hide');
       authNavbar.classList.remove('hide');
       logoutNavbar.classList.add('hide');
       authDiv.classList.remove('hide');
