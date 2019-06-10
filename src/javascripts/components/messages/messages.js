33d8795705f3fe025e75f7d9caa33a27cbcbd5d3
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-06-10 14:46:18 -0500

    wip, printing basic messages

--- src/javascripts/components/messages/messages.js
+++ src/javascripts/components/messages/messages.js
@@ -1,7 +1,29 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import messagesData from '../../helpers/data/messages-data';
+import smash from '../../helpers/smash';
 import usersData from '../../helpers/data/users-data';
+import util from '../../helpers/util';
+
+const messageViewBuilder = (arrayToPrint, currentUserId) => {
+  let domString = '';
+  arrayToPrint.forEach((message) => {
+    domString += '<div class="row">';
+    domString += '<div class="col-9">';
+    domString += `<h4 class="w-100 text-center">${message.userName}</h4>`;
+    domString += `<p class="col-3">${message.timeStamp}</p>`;
+    domString += `<p class="col-6">${message.message}</p>`;
+    domString += '</div>';
+    if (message.uid === currentUserId) {
+      domString += '<div class="col-3">';
+      domString += '<button class="edit-button">Edit</button>';
+      domString += '<button class="delete-button">Delete</button>';
+      domString += '</div>';
+    }
+    domString += '</div>';
+  });
+  util.printToDom('messagesContainer', domString);
+};
 
 const initMessages = () => {
   const currentUser = firebase.auth().currentUser.uid;
@@ -9,9 +31,8 @@ const initMessages = () => {
     .then((messages) => {
       usersData.getUsersArray()
         .then((users) => {
-          const matchingUser = users.find(u => u.uid === currentUser);
-          console.error('current user object in array', matchingUser);
-          console.error(messages, 'got messages array in init');
+          const finalMessages = smash.buildMessagesArray(users, messages);
+          messageViewBuilder(finalMessages, currentUser);
         })
         .catch();
     })
