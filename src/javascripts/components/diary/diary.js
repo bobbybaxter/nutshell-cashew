78cfab64f1494b355ac8adac439cc67148798315
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-11 20:52:26 -0500

    wip

--- src/javascripts/components/diary/diary.js
+++ src/javascripts/components/diary/diary.js
@@ -15,11 +15,18 @@ const addDiaryEntry = () => {
     entryBody: $('#diary-entry-input').val(),
     uid: firebase.auth().currentUser.uid,
   };
+  $('#diary-title-input').val('');
+  $('#diary-date-input').val('');
+  $('#diary-entry-input').val('');
   diaryData.addDiaryEntry(newEntry)
     .then(() => {
       initDiary(); // eslint-disable-line no-use-before-define
     })
     .catch(error => console.error(error));
+};
+
+const attachButtonEvents = (event) => {
+  console.error(event);
 };
 
 const writeDiaryEntries = (diaryEntries) => {
@@ -30,11 +37,14 @@ const writeDiaryEntries = (diaryEntries) => {
     domString += `    <h5 class="mb-0">${diaryEntry.title}</h5>`;
     domString += `    <div>${diaryEntry.date}</div>`;
     domString += `    <div class="my-3">${diaryEntry.entryBody}</div>`;
-    domString += '    <div class="d-flex justify-content-start"><button class="btn btn-outline-dark mr-1">Edit</button><button class="btn btn-outline-dark">Delete</button></div>';
+    domString += '    <div class="d-flex justify-content-start">';
+    domString += '      <button class="edit btn btn-outline-dark mr-1">Edit</button>';
+    domString += '      <button class="delete btn btn-outline-dark">Delete</button></div>';
     domString += '  </div>';
     domString += '</div>';
   });
   util.printToDom('diary-entries', domString);
+  attachButtonEvents();
 };
 
 const initDiary = () => {
