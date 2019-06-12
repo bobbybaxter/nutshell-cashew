c93ae863a2785e09a6415a0aa6fbbbf867d4ae15
Matt Gill <mtgill90@outlook.com>
2019-06-11 19:54:34 -0500

    wip

--- src/javascripts/components/dashboard/dashboard.js
+++ src/javascripts/components/dashboard/dashboard.js
@@ -1,10 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 // import util from '../../helpers/util';
-import dashboardData from '../../helpers/data/dashboardData';
+// import dashboardData from '../../helpers/data/dashboardData';
+
+import newsData from '../../helpers/data/newsData';
+
+// const dashboardBuilder = () => {
+//   dashboardData.getDashboardDataByUid()
+//     .then((dashboard) => {
+//       console.error(dashboard);
+//     });
+// };
 
 const dashboardBuilder = () => {
-  dashboardData.getDashboardDataByUid()
-    .then((dashboard) => {
-      console.error(dashboard);
+  const { uid } = firebase.auth().currentUser;
+  newsData.getNewsByUid(uid)
+    .then((news) => {
+      console.error(news);
     });
 };
 
