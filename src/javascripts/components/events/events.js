baa51b0f408ef42135fd4b3ada0212f44098ea44
Bob <bob.e.baxter@gmail.com>
2019-06-11 21:59:26 -0500

    update events works

--- src/javascripts/components/events/events.js
+++ src/javascripts/components/events/events.js
@@ -27,11 +27,48 @@ const addEvent = () => {
 };
 
 const deleteEvent = (e) => {
-  const eventId = e.target.id;
-  console.error(eventId);
+  const eventId = e.target.id.split('.')[1];
   eventsData.deleteEvent(eventId)
     .then(() => initEvents()) // eslint-disable-line no-use-before-define
     .catch(err => console.error(err));
+};
+
+const editEvent = (e) => {
+  const eventId = e.target.id.split('.')[1];
+  const { uid } = firebase.auth().currentUser;
+  $('#edit-event-form').modal('show');
+  eventsData.getEventsByUid(uid)
+    .then((events) => {
+      events.forEach((event) => {
+        if (event.id === eventId) {
+          $('#edit-event-name').val(event.name);
+          $('#edit-event-date').val(event.date);
+          $('#edit-event-location').val(event.location);
+          $('#edit-event-location').parent().attr('id', event.id);
+        }
+      });
+    });
+};
+
+const saveEvent = (e) => {
+  const eventId = $(e.target).parent().prev()[0].id;
+  const eventName = $('#edit-event-name').val();
+  const eventDate = $('#edit-event-date').val();
+  const eventLocation = $('#edit-event-location').val();
+  const updatedEvent = {
+    name: eventName,
+    date: eventDate,
+    location: eventLocation,
+    uid: firebase.auth().currentUser.uid,
+  };
+  eventsData.editEvent(eventId, updatedEvent)
+    .then(() => initEvents()) // eslint-disable-line no-use-before-define
+    .catch(err => console.error('event didnt update', err));
+};
+
+const eventListeners = () => {
+  $('.eventDeleteBtn').on('click', deleteEvent);
+  $('.eventEditBtn').on('click', editEvent);
 };
 
 const printEvents = (events) => {
@@ -48,17 +85,18 @@ const printEvents = (events) => {
     domString += '</tr>';
     domString += '</table>';
     domString += '<div class="d-flex flex-row">';
-    domString += '<button class="btn btn-outline-dark eventEditBtn">Edit</button>';
-    domString += `<button id="${event.id}" class="btn btn-outline-dark eventDeleteBtn">Delete</button>`;
+    domString += `<button id="edit.${event.id}" class="btn btn-outline-dark eventEditBtn">Edit</button>`;
+    domString += `<button id="delete.${event.id}" class="btn btn-outline-dark eventDeleteBtn">Delete</button>`;
     domString += '</div>';
     domString += '</div>';
   });
   util.printToDom('events', domString);
-  $('.eventDeleteBtn').on('click', deleteEvent);
+  eventListeners();
 };
 
 const initEvents = () => {
   $('#add-event').on('click', addEvent);
+  $('#save-event').on('click', saveEvent);
   const { uid } = firebase.auth().currentUser;
   eventsData.getEventsByUid(uid)
     .then(events => printEvents(events))
