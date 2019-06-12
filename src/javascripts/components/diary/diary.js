81fbb9cf1ca4efb58dfaacd1c9dbd039aac7a353
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-11 21:51:50 -0500

    populating with current entry wip

--- src/javascripts/components/diary/diary.js
+++ src/javascripts/components/diary/diary.js
@@ -15,9 +15,6 @@ const addDiaryEntry = () => {
     entryBody: $('#diary-entry-input').val(),
     uid: firebase.auth().currentUser.uid,
   };
-  $('#diary-title-input').val('');
-  $('#diary-date-input').val('');
-  $('#diary-entry-input').val('');
   diaryData.addDiaryEntry(newEntry)
     .then(() => {
       initDiary(); // eslint-disable-line no-use-before-define
@@ -25,20 +22,40 @@ const addDiaryEntry = () => {
     .catch(error => console.error(error));
 };
 
-const attachButtonEvents = (event) => {
-  console.error(event);
+const prepareEditForm = (event) => {
+  const entryId = event.target.dataset.value;
+  $('#diary-title-input').val($(`#${entryId}-title`).html());
+  $('#diary-date-input').val($(`#${entryId}-date`).html());
+  $('#diary-entry-input').val($(`#${entryId}-entry`).html());
+
+  const adjustedEntry = {
+    title: $('#diary-title-input').val(),
+    date: $('#diary-date-input').val(),
+    entryBody: $('#diary-entry-input').val(),
+    uid: firebase.auth().currentUser.uid,
+  };
+
+  diaryData.editDiaryEntry(adjustedEntry)
+    .then(() => {
+      initDiary(); // eslint-disable-line no-use-before-define
+    })
+    .catch(error => console.error(error));
+};
+
+const attachButtonEvents = () => {
+  $('.edit').click(prepareEditForm);
 };
 
 const writeDiaryEntries = (diaryEntries) => {
   let domString = '';
   diaryEntries.forEach((diaryEntry) => {
-    domString += `<div id=${diaryEntry.id} class="media mb-5">`;
+    domString += `<div id="${diaryEntry.id}" class="media mb-5">`;
     domString += '  <div class="media-body">';
-    domString += `    <h5 class="mb-0">${diaryEntry.title}</h5>`;
-    domString += `    <div>${diaryEntry.date}</div>`;
-    domString += `    <div class="my-3">${diaryEntry.entryBody}</div>`;
+    domString += `    <h5 id="${diaryEntry.id}-title" class="mb-0">${diaryEntry.title}</h5>`;
+    domString += `    <div id="${diaryEntry.id}-date">${diaryEntry.date}</div>`;
+    domString += `    <div id="${diaryEntry.id}-entry" class="my-3">${diaryEntry.entryBody}</div>`;
     domString += '    <div class="d-flex justify-content-start">';
-    domString += '      <button class="edit btn btn-outline-dark mr-1">Edit</button>';
+    domString += `      <button data-value="${diaryEntry.id}" class="edit btn btn-outline-dark mr-1" data-toggle="modal" data-target="#add-entry-form">Edit</button>`;
     domString += '      <button class="delete btn btn-outline-dark">Delete</button></div>';
     domString += '  </div>';
     domString += '</div>';
@@ -47,8 +64,15 @@ const writeDiaryEntries = (diaryEntries) => {
   attachButtonEvents();
 };
 
+const clearForm = () => {
+  $('#diary-title-input').val('');
+  $('#diary-date-input').val('');
+  $('#diary-entry-input').val('');
+};
+
 const initDiary = () => {
   document.getElementById('add-diary-entry').addEventListener('click', addDiaryEntry);
+  $('#add-entry-form').on('hidden.bs.modal', clearForm);
   const { uid } = firebase.auth().currentUser;
   diaryData.getDiaryEntriesByUid(uid)
     .then((diaryEntries) => {
