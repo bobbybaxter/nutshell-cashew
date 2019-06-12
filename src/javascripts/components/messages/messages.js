30b27df17ec66cfcd37f07a52540d1c2d9479c6f
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-06-11 22:37:30 -0500

    finished first draft create messages

--- src/javascripts/components/messages/messages.js
+++ src/javascripts/components/messages/messages.js
@@ -1,18 +1,53 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import moment from 'moment';
 import messagesData from '../../helpers/data/messages-data';
 import smash from '../../helpers/smash';
 import usersData from '../../helpers/data/users-data';
 import util from '../../helpers/util';
 import './messages.scss';
 
+const submitMessage = (e) => {
+  e.preventDefault();
+  if (e.key === 'Enter') {
+    const messageInput = document.getElementById('addMessageInput');
+    const messageTimeStamp = new Date();
+    const currentUserId = firebase.auth().currentUser.uid;
+    const newMessage = {
+      uid: currentUserId,
+      timeStamp: messageTimeStamp,
+      message: messageInput.value,
+    };
+    messagesData.getMessagesArray()
+      .then((messages) => {
+        const matchingMessage = messages.find(m => m.uid === newMessage.uid && m.timeStamp === newMessage.timeStamp);
+        if (matchingMessage) {
+          console.error('message found');
+        } else {
+          messagesData.addMessage(newMessage)
+            .then(() => {
+              messageInput.value = '';
+              initMessages(); // eslint-disable-line no-use-before-define
+            })
+            .catch();
+        }
+      })
+      .catch();
+  }
+};
+
+const addEvents = () => {
+  document.getElementById('addMessageInput').addEventListener('keyup', submitMessage);
+};
+
 const messageViewBuilder = (arrayToPrint, currentUserId) => {
   let domString = '';
   arrayToPrint.forEach((message) => {
+    const formattedTimeStamp = moment(message.timeStamp).format('MMMM D, YYYY h:mm A');
     domString += '<div class="row">';
     domString += '<div class="col-9">';
     domString += `<h4 class="w-100 text-center">${message.userName}</h4>`;
-    domString += `<p class="col-3">${message.timeStamp}</p>`;
+    domString += `<p class="col-3">${formattedTimeStamp}</p>`;
     domString += `<p class="col-6">${message.message}</p>`;
     domString += '</div>';
     if (message.uid === currentUserId) {
@@ -43,4 +78,5 @@ const initMessages = () => {
 
 export default {
   initMessages,
+  addEvents,
 };
